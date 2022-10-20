a10e031dc7d571a267f6fecb37978ba70b27065b
bre3-19 <bre3_19@alu.uabcs.mx>
2022-10-20 10:02:35 -0600

    20-10-2022

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -3,10 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
         <title></title>
     </head>
 
-    <body>
+    <!-- Agregado solo para no lastimarme la vista -->
+    <body style="background-color:black">
         <form method="post" action="http://127.0.0.1:8000/users/">
 
             @csrf
