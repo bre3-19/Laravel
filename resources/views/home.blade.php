03d6549dfb542b078c710ab229a485ae52766275
Daniel-Sanchez-111 <bre3_19@alu.uabcs.mx>
2022-11-10 10:08:22 -0700

    10-11-2022

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+    @section('contenido')
+
+        <h1>
+            Hola
+        </h1>
+
+    @endsection
+
+    @section('scripts')
+
+        <!-- Hola -->
+
+    @endsection
